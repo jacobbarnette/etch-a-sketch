9a12e2632d790783868c5c4bbcb262e68bbe60a6
Jacob Barnette <jacobcbarnette@gmail.com>
2020-11-02 20:48:43 -0500

    added evenListener to add to  grid based on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,32 @@
 let container = document.getElementById('container');
+let resetBtn = document.getElementById('resetBtn');
+let columnCount = 0;
+let gridSizeInput = document.getElementById('gridSizeInput');
 
+let gridSize = 16;
 
-for(let i =0; i < 20; i++) {
-    let row = document.createElement('div');
-        row.setAttribute('class', 'row');
-    container.appendChild(row);
-    test(row);
+function createGrid(gridSize) {
+    console.log('1' + gridSize);
+    for(let i =0; i < gridSize; i++) {
+        let row = document.createElement('div');
+            row.setAttribute('class', 'row');
+        container.appendChild(row);    
+        test(row);
+    }  
 }
 
 function test(row) {
-    for(let i =0; i < 20; i++) {
+    for(let i =0; i < gridSize; i++) {
         let column = document.createElement('div');
             column.setAttribute('class', 'column');
         row.appendChild(column);
         column.addEventListener('mouseover', function() {
-            if(!column.style.background == true) {
+            if(column.style.background == false) {
+                columnCount++
                 column.style.background = randomColor();
-            }           
+            }       
         });
-    }
+    }  
 }
 
 function randomColor () {
@@ -30,3 +38,12 @@ function randomColor () {
     return rgb;
 }
 
+
+gridSizeInput.addEventListener('click', function() {
+   let gridSize = prompt('How big of a grid ya wnat?');
+   console.log(gridSize)
+    createGrid(gridSize);
+})
+
+console.log(gridSize);
+createGrid(gridSize);
